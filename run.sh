a68db81f89894ca04ce76891ff8ba79ec5e6ba73
arturozv <zzarturozz@gmail.com>
2017-06-06 08:57:38 -0700

    run shell

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+./mvnw clean test install
+echo "build success, running the program..."
+
+java -server -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -XX:ParallelGCThreads=4 -XX:+PrintGC -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:gc.log \
+-jar ./target/translate-files.jar
