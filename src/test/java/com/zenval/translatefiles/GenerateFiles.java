b290bfac737b473d4e69f3b4110f85043f28e7b2
arturozv <zzarturozz@gmail.com>
2017-06-07 12:12:46 -0700

    generate test files without last linebreak

--- src/test/java/com/zenval/translatefiles/GenerateFiles.java
+++ src/test/java/com/zenval/translatefiles/GenerateFiles.java
@@ -14,10 +14,15 @@ public class GenerateFiles {
 
     @Test
     public void generateTestFiles() throws Exception {
+        int maxLines = 16;
         for (int file = 1; file <= 4; file++) {
             try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("file" + file), "utf-8"))) {
-                for (int word = 1; word <= 16 - file; word++) {
-                    writer.write("file" + file + "word" + word + "\n");
+                for (int word = 1; word <= maxLines - file; word++) {
+                    String text = "file" + file + "word" + word;
+                    if (word != maxLines - file) {
+                        text += "\n";
+                    }
+                    writer.write(text);
                 }
             }
         }
