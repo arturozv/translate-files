17e985ad3cc6d268276c93d8dc1f3d7a1c343357
arturozv <zzarturozz@gmail.com>
2017-06-07 16:28:36 -0700

    batchFileWriter test

---
+++ src/test/java/com/zenval/translatefiles/job/components/BatchFileWriterTest.java
@@ -0,0 +1,60 @@
+package com.zenval.translatefiles.job.components;
+
+import com.zenval.translatefiles.dto.BatchGroup;
+import com.zenval.translatefiles.dto.Translation;
+import com.zenval.translatefiles.service.BatchAggregator;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.batch.item.ItemWriter;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class BatchFileWriterTest {
+
+    @Mock
+    private ItemWriter<String> itemWriter;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void writer_calls_aggregator() throws Exception {
+        //given
+        Translation translation = new Translation("text", "translated", 1l, "file1");
+        BatchAggregator batchAggregator = mock(BatchAggregator.class);
+        BatchFileWriter batchFileWriter = new BatchFileWriter(batchAggregator, itemWriter);
+
+        //when
+        batchFileWriter.write(Collections.singletonList(translation));
+
+        //then
+        verify(batchAggregator, times(1)).aggregate(eq(translation));
+    }
+
+    @Test
+    public void write_batch_calls_writer() throws Exception {
+        //given
+        BatchAggregator batchAggregator = mock(BatchAggregator.class);
+        BatchGroup batchGroup = new BatchGroup(1l, 1l);
+        batchGroup.getTranslations().addAll(Arrays.asList(new Translation("text", "translated", 1l, "file1"), new Translation("text2", "translated2", 1l, "file2")));
+        BatchFileWriter batchFileWriter = new BatchFileWriter(batchAggregator, itemWriter);
+
+        //when
+        batchFileWriter.writeBatch(batchGroup);
+
+        //then
+        verify(itemWriter, times(1)).write(Arrays.asList("translated", "translated2"));
+    }
+
+}
