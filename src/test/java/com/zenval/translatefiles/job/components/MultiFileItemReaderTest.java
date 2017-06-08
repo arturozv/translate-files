e38fe555e51fcfb4647784510634bc0c30b1b923
arturozv <zzarturozz@gmail.com>
2017-06-08 10:49:18 -0700

    Multi File Reader Test

--- src/test/java/com/zenval/translatefiles/job/components/MultiFileItemReaderTest.java
+++ src/test/java/com/zenval/translatefiles/job/components/MultiFileItemReaderTest.java
@@ -27,27 +27,44 @@ public class MultiFileItemReaderTest {
     @Test
     public void file_reads_multiple_lines() throws Exception {
         //given
-        File file1 = testFolder.newFile("file1.txt");
-        File file2 = testFolder.newFile("file2.txt");
-        File file3 = testFolder.newFile("file3.txt");
+        File file1 = testFolder.newFile("file1.txt.translated");
+        File file2 = testFolder.newFile("file2.txt.translated");
+        File file3 = testFolder.newFile("file3.txt.translated");
 
         FileUtils.writeStringToFile(file1, "f1line1\nf1line2", Charset.defaultCharset());
         FileUtils.writeStringToFile(file2, "f2line1\nf2line2", Charset.defaultCharset());
-        FileUtils.writeStringToFile(file3, "f3line1\nf3line2", Charset.defaultCharset());
+        FileUtils.writeStringToFile(file3, "f3line1\nf3line2\nf3line3", Charset.defaultCharset());
 
         List<File> all = Arrays.asList(file1, file2, file3);
-        List<String> paths = all.stream().map(File::getPath).collect(Collectors.toList());
+        List<String> paths = all.stream().map(f-> f.getPath().replace(".translated", "")).collect(Collectors.toList());
 
         Files files = mock(Files.class);
         when(files.getPaths()).thenReturn(paths);
 
         MultiFileItemReader multiFileItemReader = new MultiFileItemReader(files);
+        multiFileItemReader.init();
 
         //when
         List<String> firstLine = multiFileItemReader.read();
-
         //then
         assertThat(firstLine).hasSize(3);
         assertThat(firstLine).containsExactly("f1line1", "f2line1", "f3line1");
+
+        //when
+        List<String> secondLine = multiFileItemReader.read();
+        //then
+        assertThat(secondLine).hasSize(3);
+        assertThat(secondLine).containsExactly("f1line2", "f2line2", "f3line2");
+
+        //when
+        List<String> thirdLine = multiFileItemReader.read();
+        //then
+        assertThat(thirdLine).hasSize(1);
+        assertThat(thirdLine).containsExactly("f3line3");
+
+        //when
+        List<String> forthLine = multiFileItemReader.read();
+        //then
+        assertThat(forthLine).isNull();
     }
 }
